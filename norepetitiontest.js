68cb87bbe4d8ce76d1618bf45453bb04dc7e62c5
gabriel <gabriel.ngige@student.moringaschool.com>
2026-03-18 17:55:33 +0300

    commit webpage_assignment/fireisland branch.  Changes to be committed: 	new file:   webpage_assignment.html

--- norepetitiontest.js
+++ norepetitiontest.js
@@ -1,9 +1,10 @@
 //figure our how to output sdft17 without repeating the code multiple times using indexing.
 
-let sdft17 = ["gabriel", "john", "michael" ]
+let sdft17 = ["gabriel", "john", "michael", "sarah" ]
 console.log(sdft17[0]); // Output: "gabriel"
 console.log(sdft17[1]); // Output: "john"
 console.log(sdft17[2]); // Output: "michael"
+console.log(sdft17[3]); // Output: "sarah"
 
 //or we can use a loop to output all the elements in the array without repeating the code multiple times.
 
